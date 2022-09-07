fddeff082cd520694e11e170f6010c000028e123
Cristian G P <criperez@frba.utn.edu.ar>
2022-09-07 20:41:28 -0300

    V1.2

--- app/src/main/java/com/example/writer3/MainActivity.kt
+++ app/src/main/java/com/example/writer3/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("CRistian")
         println("Gonzalo")
-        println("Perez2")
+        println("Perez4")
     }
-
+    println("RAmpelini")
 }
